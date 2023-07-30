30f0d3a0f28d3ebb2acd5011d2134720df6b8916
James Koonts <jkoontsiii@gmail.com>
2023-07-29 22:09:05 -0400

    .

--- config/up.sql
+++ config/up.sql
@@ -10,16 +10,6 @@ CREATE TABLE `readings` (
     `main_pollutant` varchar(255) NOT NULL,
     PRIMARY KEY (`id`));
 
-CREATE TABLE `readings` (
-`city` varchar(255) NOT NULL,
-`state` varchar(255) NOT NULL,
-`temp` varchar(255) NOT NULL,
-`pressure` varchar(255) NOT NULL,
-`humidity` varchar(255) NOT NULL,
-`wind_speed` varchar(255) NOT NULL,
-`current_pollution_aqius` varchar(255) NOT NULL,
-`main_pollutant` varchar(255) NOT NULL,
-PRIMARY KEY (`id`));
 
 CREATE TABLE `subs` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
