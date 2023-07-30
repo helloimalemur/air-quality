5e7a7f6ecffcffda4067217443d62dccdb08fa6c
James Koonts <jkoontsiii@gmail.com>
2023-07-29 21:16:12 -0400

    .

--- src/main.rs
+++ src/main.rs
@@ -88,36 +88,6 @@ async fn addaq(
 // // // // // // // // // // // // // // // // // // // // // // // //
 // // // // // // // // // // // // // // // // // // // // // // // //
 
-// pub struct ApiKey<'r>(&'r str);
-//
-// #[derive(Debug)]
-// pub enum ApiKeyError {
-//     MissingError,
-//     InvalidError,
-// }
-//
-// #[rocket::async_trait]
-// impl<'r> FromRequest<'r> for ApiKey<'r> {
-//     type Error = ApiKeyError;
-//
-//     async fn from_request(req: &'r Request<'_>) -> Outcome<ApiKey<'r>, (Status, ApiKeyError), ()> {
-//         /// Returns true if `key` is a valid API key string.
-//         fn is_valid(key: &str) -> bool {
-//             key == "yourapikey"
-//         }
-//
-//         match req.headers().get_one("x-api-key") {
-//             None => Outcome::Failure((Status::BadRequest, ApiKeyError::MissingError)),
-//             Some(key) if is_valid(key) => Outcome::Success(ApiKey(key)),
-//             Some(_) => Outcome::Failure((Status::BadRequest, ApiKeyError::InvalidError)),
-//         }
-//     }
-// }
-
-
-// // // // // // // // // // // // // // // // // // // // // // // //
-// // // // // // // // // // // // // // // // // // // // // // // //
-
 
 pub struct CORS;
 
